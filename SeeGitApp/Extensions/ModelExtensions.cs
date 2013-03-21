d622f71ea3a83e65f9c73bc334c5bde84b5a7f07
Wesley McClure <wes.mcclure@gmail.com>
2013-03-20 20:51:14 -0500

    Monitor for deleted files too even though this really shouldn't happen :)

--- SeeGitApp/Extensions/ModelExtensions.cs
+++ SeeGitApp/Extensions/ModelExtensions.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.IO;
-using System.Reactive.Linq;
-
-namespace SeeGit
+﻿namespace SeeGit
 {
+    using System;
+    using System.IO;
+    using System.Reactive.Linq;
+    using BclExtensionMethods;
+
     public static class ModelExtensions
     {
         private const string GitDirectoryName = ".git";
@@ -23,7 +24,7 @@ namespace SeeGit
             if (path == null) throw new ArgumentNullException("path");
 
             //If we are passed a .git directory, just return it straightaway
-            DirectoryInfo pathDirectoryInfo = new DirectoryInfo(path);
+            var pathDirectoryInfo = new DirectoryInfo(path);
             if (pathDirectoryInfo.Name == GitDirectoryName)
             {
                 return path;
@@ -31,11 +32,11 @@ namespace SeeGit
 
             if (!pathDirectoryInfo.Exists) return Path.Combine(path, GitDirectoryName);
 
-            DirectoryInfo checkIn = pathDirectoryInfo;
+            var checkIn = pathDirectoryInfo;
 
             while (checkIn != null)
             {
-                string pathToTest = Path.Combine(checkIn.FullName, GitDirectoryName);
+                var pathToTest = Path.Combine(checkIn.FullName, GitDirectoryName);
                 if (Directory.Exists(pathToTest))
                 {
                     return pathToTest;
@@ -53,30 +54,30 @@ namespace SeeGit
 
         public static IObservable<FileSystemEventArgs> CreateGitRepositoryCreationObservable(string path)
         {
-            string expectedGitDirectory = Path.Combine(path, GitDirectoryName);
+            var expectedGitDirectory = Path.Combine(path, GitDirectoryName);
             return new FileSystemWatcher(path)
-                   {
-                       IncludeSubdirectories = false,
-                       EnableRaisingEvents = true,
-                       NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.DirectoryName
-                   }.ObserveFileSystemCreateEvents()
-                .Where(
-                    e =>
-                    e.ChangeType == WatcherChangeTypes.Created &&
-                    e.FullPath.Equals(expectedGitDirectory, StringComparison.OrdinalIgnoreCase))
-                .Throttle(TimeSpan.FromMilliseconds(250));
+                {
+                    IncludeSubdirectories = false,
+                    EnableRaisingEvents = true,
+                    NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.DirectoryName
+                }.ObserveFileSystemCreateEvents()
+                 .Where(
+                     e =>
+                     e.ChangeType.In(WatcherChangeTypes.Created, WatcherChangeTypes.Deleted) &&
+                     e.FullPath.Equals(expectedGitDirectory, StringComparison.OrdinalIgnoreCase))
+                 .Throttle(TimeSpan.FromMilliseconds(250));
             // todo perhaps we want a small throttle window and reset the window each time we get a change notification, like a BufferUntilCalm
         }
 
         public static IObservable<FileSystemEventArgs> CreateGitRepositoryChangesObservable(string path)
         {
             return new FileSystemWatcher(path)
-                   {
-                       IncludeSubdirectories = true,
-                       EnableRaisingEvents = true,
-                       NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.LastWrite
-                   }.ObserveFileSystemChangeEvents()
-                .Throttle(TimeSpan.FromMilliseconds(250));
+                {
+                    IncludeSubdirectories = true,
+                    EnableRaisingEvents = true,
+                    NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.LastWrite
+                }.ObserveFileSystemChangeEvents()
+                 .Throttle(TimeSpan.FromMilliseconds(250));
             // todo perhaps we want a small throttle window and reset the window each time we get a change notification, like a BufferUntilCalm
         }
     }
