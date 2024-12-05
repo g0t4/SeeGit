07268c5d3aa82334b755d848c05e57a67748ff72
Wes Higbee <wes.mcclure@gmail.com>
2024-12-05 13:50:32 -0600

    fix enum exception after removing an item, blew up the highlighting (removal)

--- SeeGitApp/Models/ReachableHighlightAlgorithm.cs
+++ SeeGitApp/Models/ReachableHighlightAlgorithm.cs
@@ -1,14 +1,14 @@
 ﻿using GraphShape.Algorithms.Highlight;
 using QuikGraph;
+using System.Linq;
 
 namespace SeeGit.Models
 {
     public class ReachableHighlightAlgorithm<TVertex, TEdge, TGraph> :
-        HighlightAlgorithmBase<TVertex, TEdge, TGraph, IHighlightParameters> where TVertex : class
-                                                                             where TEdge : IEdge<TVertex>
-                                                                             where TGraph : class, IBidirectionalGraph<TVertex, TEdge>
+            HighlightAlgorithmBase<TVertex, TEdge, TGraph, IHighlightParameters> where TVertex : class
+                                                                                 where TEdge : IEdge<TVertex>
+                                                                                 where TGraph : class, IBidirectionalGraph<TVertex, TEdge>
     {
-        // Methods
         public ReachableHighlightAlgorithm(IHighlightController<TVertex, TEdge, TGraph> controller,
                                            IHighlightParameters parameters) : base(controller, parameters)
         {
@@ -16,36 +16,30 @@ namespace SeeGit.Models
 
         private void ClearAllHighlights()
         {
-            // FYI reachable highlights are not quite working yet though not that big of a deal IMO.. just the blue line when hovering over the graph, so lets focus on why layout isn't working quite right first
-            //   TODO blue reachables are pointed at wrong shapes and not always updating as they would in v1.0.1/v1.1
-            //   ALSO, not removing blue highlight when hover off of vertex... unlike original v1.0.1 which snapped off right away
             //   IMPROVEMENT? take off black highlight of arrow if hover over arrow (edge)
+            // TODO how can I style the highlighted colors / etc... is blue just a default on the edges? 
+            //  TODO style the selected commit vertices, and others?
 
-            // OMG do I need to lock before clearing and removing items?
-            ClearSemiHighlights();
-            // TODO what are all the warnings here about modified collections and cannot finish enumeration, add those back (user unhandled), right now I ignored them just to test app out
-            foreach (TVertex local in Controller.HighlightedVertices)
-            {
-                Controller.RemoveHighlightFromVertex(local);
-            }
-            foreach (TEdge local2 in Controller.HighlightedEdges)
-            {
-                Controller.RemoveHighlightFromEdge(local2);
-            }
-        }
+            // FTR, added ToList() to avoid InvalidOperationException after first remove which breaks removing highlights and thus highlights break, ToList() isn't lazy enumerated, it's a cached list then and then enumeration is over the cached list so when the underlying collection is altered enumeration isn't Fubar'd
 
-        private void ClearSemiHighlights()
-        {
-            // TODO what are all the warnings here about modified collections and cannot finish enumeration, add those back (user unhandled), right now I ignored them just to test app out
-            //   FYI definitely happened often on SemiHighlightedVertices:
-            // IIRC it was InvalidOperationException
-            foreach (var vertex in Controller.SemiHighlightedVertices) // HOW DO I add back the user unhandled exception breaking here (not breakpoint)
+            // *** semi
+            foreach (var vertex in Controller.SemiHighlightedVertices.ToList())
             {
                 Controller.RemoveSemiHighlightFromVertex(vertex);
             }
-            foreach (var edge in Controller.SemiHighlightedEdges)
+            foreach (var edge in Controller.SemiHighlightedEdges.ToList())
             {
                 Controller.RemoveSemiHighlightFromEdge(edge);
+            }
+
+            // *** full on
+            foreach (var vertex in Controller.HighlightedVertices.ToList())
+            {
+                Controller.RemoveHighlightFromVertex(vertex);
+            }
+            foreach (var edge in Controller.HighlightedEdges.ToList())
+            {
+                Controller.RemoveHighlightFromEdge(edge);
             }
         }
 
@@ -73,7 +67,8 @@ namespace SeeGit.Models
             if (!Controller.Graph.IsDirected) return false;
             ClearAllHighlights();
 
-            if (vertex == null || !Controller.Graph.ContainsVertex(vertex)) return false;
+            if (vertex == null || !Controller.Graph.ContainsVertex(vertex))
+                return false;
 
             Controller.HighlightVertex(vertex, "Source");
             return HighlightChildren(vertex);
